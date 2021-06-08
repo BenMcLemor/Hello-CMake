86edc2fecceed012c5a68abb7d3d79e194eb9ac9
Ben Mclemor <benmclemor2019@gmail.com>
2021-06-08 13:35:12 +0200

    second Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/atc/Documents/CMakeTutorials/Hello World
+# Build directory: C:/Users/atc/Documents/CMakeTutorials/Hello World/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[test1]=] "C:/Users/atc/Documents/CMakeTutorials/Hello World/build/HelloWorld")
+set_tests_properties([=[test1]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/atc/Documents/CMakeTutorials/Hello World/CMakeLists.txt;24;add_test;C:/Users/atc/Documents/CMakeTutorials/Hello World/CMakeLists.txt;0;")
